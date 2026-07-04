fb2edbbba0b27cb7ce3c82452bce0f0b9bca69c2
jbedicionesdigitales <jorgebrito@outlook.com.ar>
2026-07-04 20:30:10 -0300

    Update config.js

--- config.js
+++ config.js
@@ -15,25 +15,25 @@ const DEFAULT_CONFIG = {
       title: "Cocina Saludable by JB",
       description: "Recetas saludables, simples y prácticas para todos los días.",
       image: "imagenes/cocina-saludable.jpg",
-      pdf: "pdf/cocina-saludable-by-jb.pdf"
+      pdf: "pdf/cocina-saludable-by-jb-obtimizado.pdf"
     },
     {
       title: "Pastelería Profesional by Ile",
       description: "Pastelería, técnicas, decoración y recetas para emprender.",
       image: "imagenes/pasteleria-profesional.jpg",
-      pdf: "pdf/pasteleria-profesional-by-ile.pdf"
+      pdf: "pdf/pasteleria-profesional-by-ile-obtimizado.pdf"
     },
     {
       title: "Galletitas Estilo New York",
       description: "Cookies premium, gigantes, rellenas y comerciales.",
       image: "imagenes/galletitas-new-york.jpg",
-      pdf: "pdf/galletitas-new-york.pdf"
+      pdf: "pdf/galletitas-new-york-obtimizado.pdf"
     },
     {
       title: "Recetas Sin TACC by JB",
       description: "Opciones aptas, fáciles y deliciosas para ampliar tu cocina.",
       image: "imagenes/recetas-sin-tacc.jpg",
-      pdf: "pdf/recetas-sin-tacc-by-jb.pdf"
+      pdf: "pdf/recetas-sin-tacc-by-jb-obtimizado.pdf"
     }
   ]
 };
