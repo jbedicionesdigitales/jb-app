c4dd99dd24b9699d31fc7fb812422ba12c752c0c
jbedicionesdigitales <jorgebrito@outlook.com.ar>
2026-07-05 06:29:58 -0300

    Change fetch URL from '/functions/ask' to '/ask'

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ async function askAI() {
 
   chat.innerHTML += `<div class="msg user">${question}</div>`;
 
-  const res = await fetch("/functions/ask", {    
+  const res = await fetch("/ask", {    
     method: "POST",
     headers: {
       "Content-Type": "application/json"
