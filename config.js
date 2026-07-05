78649b989419a1ae203edfc736a5b6364e7bc59f
jbedicionesdigitales <jorgebrito@outlook.com.ar>
2026-07-04 21:18:42 -0300

    Fix PDF paths in config.js

--- config.js
+++ config.js
@@ -15,28 +15,28 @@ const DEFAULT_CONFIG = {
       title: "Cocina Saludable by JB",
       description: "Recetas saludables, simples y prácticas para todos los días.",
       image: "imagenes/cocina-saludable.jpg",
-      pdf: "pdf/cocina-saludable-by-jb-optimizado.pdf"
+      pdf: "pdf/cocina-saludable-by-jb-optimizado.pdf",
       shopify: "https://e-books-ediciones-jb.myshopify.com/products/e-book-cocina-saludable-by-jb",
   },
     {
       title: "Pastelería Profesional by Ile",
       description: "Pastelería, técnicas, decoración y recetas para emprender.",
       image: "imagenes/pasteleria-profesional.jpg",
-      pdf: "pdf/pasteleria-profesional-by-ile-obtimizado.pdf"
+      pdf: "pdf/pasteleria-profesional-by-ile-obtimizado.pdf",
       shopify: "https://e-books-ediciones-jb.myshopify.com/products/e-book-pasteleria-profesional-by-ile",    
   },
     {
       title: "Galletitas Estilo New York",
       description: "Cookies premium, gigantes, rellenas y comerciales.",
       image: "imagenes/galletitas-new-york.jpg",
-      pdf: "pdf/galletitas-new-york-obtimizado.pdf"
+      pdf: "pdf/galletitas-new-york-obtimizado.pdf",
       shopify: "https://e-books-ediciones-jb.myshopify.com/products/e-book-galletitas-estilo-new-york-by-ile",    
   },
     {
       title: "Recetas Sin TACC by JB",
       description: "Opciones aptas, fáciles y deliciosas para ampliar tu cocina.",
       image: "imagenes/recetas-sin-tacc.jpg",
-      pdf: "pdf/recetas-sin-tacc-by-jb-obtimizado.pdf"
+      pdf: "pdf/recetas-sin-tacc-by-jb-obtimizado.pdf",
       shopify: "https://e-books-ediciones-jb.myshopify.com/products/e-book-recetas-sin-tacc-by-jb",    
   }
   ]
